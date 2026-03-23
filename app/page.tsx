51aafc1bceaf0f1a113b6d9ccbdc59db95c79b4b
AITELHADJ <ZAITELHADJ-PRETATAIRE@atlasformen.com>
2026-03-23 16:04:35 +0100

    empty

--- app/page.tsx
+++ app/page.tsx
@@ -242,6 +242,7 @@ export default function Home() {
         </div>
       </nav>
 
+
       {/* Hero Section */}
       <section className="pt-24 pb-16 px-4 sm:px-6 lg:px-8">
         <div className="max-w-6xl mx-auto">
